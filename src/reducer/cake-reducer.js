f855abd7e62a58494b8b547f2fad685927a53f01
ASRomdon <dablegcoblan@gmail.com>
2020-08-31 00:46:09 +0700

    fix(*): update dispatch buyKelepon()

--- src/reducer/cake-reducer.js
+++ src/reducer/cake-reducer.js
@@ -1,6 +1,7 @@
-const { createStore } = require("redux")
+const { createStore, combineReducers } = require("redux")
 
 const BUY_CAKE = 'BUY_CAKE'
+const BUY_KELEPON = 'BUY_KELEPON'
 
 function buyCake() {
     return {
@@ -9,25 +10,56 @@ function buyCake() {
     }
 }
 
-const initialState = {
+function buyKelepon() {
+    return {
+        type: BUY_KELEPON,
+        info: 'Buy Kelepon'
+    }
+}
+
+const initialCakeState = {
     numOfCakes: 10
 }
 
-const reducer = (state = initialState, action) => {
+const initialKeleponState = {
+    numOfKelepon: 20
+}
+
+//(previousState, action) => newState
+const cakeReducer = (state = initialCakeState, action) => {
     switch (action.type) {
         case BUY_CAKE: return {
             ...state,
-            numOfCakes: state.numOfCakes + 1
+            numOfCakes: state.numOfCakes - 1
         }
 
         default: return state
     }
 }
 
-const store = createStore(reducer)
+const keleponReducer = (state = initialKeleponState, action) => {
+    switch (action.type) {
+        case BUY_KELEPON: return {
+            ...state,
+            numOfKelepon: state.numOfKelepon - 1
+        }
+
+        default: return state
+    }
+}
+
+//Combine Reducer
+const rootReducer = combineReducers({
+    kelepon: keleponReducer,
+    cake: cakeReducer
+})
+
+const store = createStore(rootReducer)
 console.log('Initial state', store.getState())
 const unsubscribe = store.subscribe(() => console.log('Update state', store.getState()))
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+store.dispatch(buyKelepon())
+store.dispatch(buyKelepon())
 unsubscribe()
